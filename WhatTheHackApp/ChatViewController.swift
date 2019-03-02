f1a4428c6d1cb3a1448c45a21a281ca924a2c42c
Guillermo Guridi <erpheus@gmail.com>
2019-03-02 17:28:42 +0100

    chat!!!!

--- WhatTheHackApp/ChatViewController.swift
+++ WhatTheHackApp/ChatViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ChatViewController: UIViewController {
+    
+    var heightCounter: CGFloat = 70
+    var messages = ["🍟🍔","🔊🎶",""]
+    var currentMessage = 0
 
+    @IBOutlet weak var messageField: EmojiTextField!
     @IBOutlet weak var typingTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +27,81 @@ class ChatViewController: UIViewController {
             action: #selector(dismissKeyboard))
         
         view.addGestureRecognizer(tap)
+    }
+    
+    @IBAction func sendNewMessage(_ sender: Any) {
+        
+        showOutgoingMessage(text: messageField.text!)
+        messageField.text = ""
+    }
+    
+    func showOutgoingMessage(text: String) {
+        let label =  UILabel()
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .white
+        label.text = text
+        
+        let constraintRect = CGSize(width: 0.66 * view.frame.width,
+                                    height: .greatestFiniteMagnitude)
+        let boundingBox = text.boundingRect(with: constraintRect,
+                                            options: .usesLineFragmentOrigin,
+                                            attributes: [.font: label.font],
+                                            context: nil)
+        label.frame.size = CGSize(width: ceil(boundingBox.width),
+                                  height: ceil(boundingBox.height))
+        
+        let bubbleSize = CGSize(width: label.frame.width + 28,
+                                height: label.frame.height + 20)
+        
+        let bubbleView = BubbleView()
+        bubbleView.frame.size = bubbleSize
+        bubbleView.backgroundColor = .clear
+        bubbleView.frame = CGRect(x: view.frame.width - 15 - bubbleSize.width, y: 30 + heightCounter, width: bubbleSize.width, height: bubbleSize.height)
+        view.addSubview(bubbleView)
+        
+        label.center = bubbleView.center
+        view.addSubview(label)
+        
+        heightCounter = heightCounter + bubbleSize.height + 10
+        
+        let when = DispatchTime.now() + 1.5
+        DispatchQueue.main.asyncAfter(deadline: when){
+            self.showIncomingMessage(text: self.messages[self.currentMessage])
+            self.currentMessage = self.currentMessage + 1
+        }
+    }
+    
+    func showIncomingMessage(text: String) {
+        let label =  UILabel()
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .white
+        label.text = text
+        
+        let constraintRect = CGSize(width: 0.66 * view.frame.width,
+                                    height: .greatestFiniteMagnitude)
+        let boundingBox = text.boundingRect(with: constraintRect,
+                                            options: .usesLineFragmentOrigin,
+                                            attributes: [.font: label.font],
+                                            context: nil)
+        label.frame.size = CGSize(width: ceil(boundingBox.width),
+                                  height: ceil(boundingBox.height))
+        
+        let bubbleSize = CGSize(width: label.frame.width + 28,
+                                height: label.frame.height + 20)
+        
+        let bubbleView = BubbleView()
+        bubbleView.isIncoming = true;
+        bubbleView.frame.size = bubbleSize
+        bubbleView.backgroundColor = .clear
+        bubbleView.frame = CGRect(x: 15, y: 30 + heightCounter, width: bubbleSize.width, height: bubbleSize.height)
+        view.addSubview(bubbleView)
+        
+        label.center = bubbleView.center
+        view.addSubview(label)
+        
+        heightCounter = heightCounter + bubbleSize.height + 10
     }
     
     func setupViewResizerOnKeyboardShown() {
@@ -89,4 +169,57 @@ class EmojiTextField: UITextField {
         }
         return nil
     }
+}
+
+
+class BubbleView: UIView {
+    
+    var isIncoming = false
+    
+    var incomingColor = UIColor(white: 0.9, alpha: 1)
+    var outgoingColor = UIColor(red: 0.09, green: 0.54, blue: 1, alpha: 1)
+    
+    override func draw(_ rect: CGRect) {
+        let width = rect.width
+        let height = rect.height
+        
+        let bezierPath = UIBezierPath()
+        
+        if isIncoming {
+            bezierPath.move(to: CGPoint(x: 22, y: height))
+            bezierPath.addLine(to: CGPoint(x: width - 17, y: height))
+            bezierPath.addCurve(to: CGPoint(x: width, y: height - 17), controlPoint1: CGPoint(x: width - 7.61, y: height), controlPoint2: CGPoint(x: width, y: height - 7.61))
+            bezierPath.addLine(to: CGPoint(x: width, y: 17))
+            bezierPath.addCurve(to: CGPoint(x: width - 17, y: 0), controlPoint1: CGPoint(x: width, y: 7.61), controlPoint2: CGPoint(x: width - 7.61, y: 0))
+            bezierPath.addLine(to: CGPoint(x: 21, y: 0))
+            bezierPath.addCurve(to: CGPoint(x: 4, y: 17), controlPoint1: CGPoint(x: 11.61, y: 0), controlPoint2: CGPoint(x: 4, y: 7.61))
+            bezierPath.addLine(to: CGPoint(x: 4, y: height - 11))
+            bezierPath.addCurve(to: CGPoint(x: 0, y: height), controlPoint1: CGPoint(x: 4, y: height - 1), controlPoint2: CGPoint(x: 0, y: height))
+            bezierPath.addLine(to: CGPoint(x: -0.05, y: height - 0.01))
+            bezierPath.addCurve(to: CGPoint(x: 11.04, y: height - 4.04), controlPoint1: CGPoint(x: 4.07, y: height + 0.43), controlPoint2: CGPoint(x: 8.16, y: height - 1.06))
+            bezierPath.addCurve(to: CGPoint(x: 22, y: height), controlPoint1: CGPoint(x: 16, y: height), controlPoint2: CGPoint(x: 19, y: height))
+            
+            incomingColor.setFill()
+            
+        } else {
+            bezierPath.move(to: CGPoint(x: width - 22, y: height))
+            bezierPath.addLine(to: CGPoint(x: 17, y: height))
+            bezierPath.addCurve(to: CGPoint(x: 0, y: height - 17), controlPoint1: CGPoint(x: 7.61, y: height), controlPoint2: CGPoint(x: 0, y: height - 7.61))
+            bezierPath.addLine(to: CGPoint(x: 0, y: 17))
+            bezierPath.addCurve(to: CGPoint(x: 17, y: 0), controlPoint1: CGPoint(x: 0, y: 7.61), controlPoint2: CGPoint(x: 7.61, y: 0))
+            bezierPath.addLine(to: CGPoint(x: width - 21, y: 0))
+            bezierPath.addCurve(to: CGPoint(x: width - 4, y: 17), controlPoint1: CGPoint(x: width - 11.61, y: 0), controlPoint2: CGPoint(x: width - 4, y: 7.61))
+            bezierPath.addLine(to: CGPoint(x: width - 4, y: height - 11))
+            bezierPath.addCurve(to: CGPoint(x: width, y: height), controlPoint1: CGPoint(x: width - 4, y: height - 1), controlPoint2: CGPoint(x: width, y: height))
+            bezierPath.addLine(to: CGPoint(x: width + 0.05, y: height - 0.01))
+            bezierPath.addCurve(to: CGPoint(x: width - 11.04, y: height - 4.04), controlPoint1: CGPoint(x: width - 4.07, y: height + 0.43), controlPoint2: CGPoint(x: width - 8.16, y: height - 1.06))
+            bezierPath.addCurve(to: CGPoint(x: width - 22, y: height), controlPoint1: CGPoint(x: width - 16, y: height), controlPoint2: CGPoint(x: width - 19, y: height))
+            
+            outgoingColor.setFill()
+        }
+        
+        bezierPath.close()
+        bezierPath.fill()
+    }
+    
 }
