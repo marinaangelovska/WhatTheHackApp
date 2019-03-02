033a5f300d8c70fa5a9a6f561dd37face9e4c903
Guillermo Guridi <erpheus@gmail.com>
2019-03-02 03:56:50 +0100

    Some style changes

--- WhatTheHackApp/ViewController.swift
+++ WhatTheHackApp/ViewController.swift
@@ -13,13 +13,30 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var chatButton: UIButton!
     @IBOutlet weak var foundButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var shadowView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+        //tableView.delegate = self
+        //tableView.dataSource = self
         makeButtonRound(button: chatButton)
         makeButtonRound(button: foundButton)
+        let topShadow = EdgeShadowLayer(
+            forView: shadowView,
+            edge: .Top,
+            shadowRadius: 10,
+            toColor: UIColor.init(red: 0.2, green: 0.2, blue: 0.2, alpha: 0),
+            fromColor: UIColor.init(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.15)
+        )
+        //shadowView.layer.addSublayer(topShadow)
+        let bottomShadow = EdgeShadowLayer(
+            forView: shadowView,
+            edge: .Bottom,
+            shadowRadius: 10,
+            toColor: UIColor.init(red: 0.2, green: 0.2, blue: 0.2, alpha: 0),
+            fromColor: UIColor.init(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.15)
+        )
+        shadowView.layer.addSublayer(bottomShadow)
     }
     
     //MARK: TableView functions
@@ -38,14 +55,59 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
         button.backgroundColor = .clear
         button.layer.cornerRadius = 7
-        button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 3
+        button.layer.borderColor = button.titleLabel?.textColor.cgColor;
         
     }
 
     //MARK: IBActions
     
     @IBAction func chatButtonPressed(_ sender: Any) {
+    }
+}
+
+public class EdgeShadowLayer: CAGradientLayer {
+    
+    public enum Edge {
+        case Top
+        case Left
+        case Bottom
+        case Right
+    }
+    
+    public init(forView view: UIView,
+                edge: Edge = Edge.Top,
+                shadowRadius radius: CGFloat = 20.0,
+                toColor: UIColor = UIColor.white,
+                fromColor: UIColor = UIColor.black) {
+        super.init()
+        self.colors = [fromColor.cgColor, toColor.cgColor]
+        self.shadowRadius = radius
+        
+        let viewFrame = view.frame
+        
+        switch edge {
+        case .Top:
+            startPoint = CGPoint(x: 0.5, y: 0.0)
+            endPoint = CGPoint(x: 0.5, y: 1.0)
+            self.frame = CGRect(x: 0.0, y: 0.0, width: viewFrame.width, height: shadowRadius)
+        case .Bottom:
+            startPoint = CGPoint(x: 0.5, y: 1.0)
+            endPoint = CGPoint(x: 0.5, y: 0.0)
+            self.frame = CGRect(x: 0.0, y: viewFrame.height - shadowRadius, width: viewFrame.width, height: shadowRadius)
+        case .Left:
+            startPoint = CGPoint(x: 0.0, y: 0.5)
+            endPoint = CGPoint(x: 1.0, y: 0.5)
+            self.frame = CGRect(x: 0.0, y: 0.0, width: shadowRadius, height: viewFrame.height)
+        case .Right:
+            startPoint = CGPoint(x: 1.0, y: 0.5)
+            endPoint = CGPoint(x: 0.0, y: 0.5)
+            self.frame = CGRect(x: viewFrame.width - shadowRadius, y: 0.0, width: shadowRadius, height: viewFrame.height)
+        }
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
